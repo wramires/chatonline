6b49e913ad84e974e36f24f0dbeb025c2e03e648
Wilson Ramires <84689156+wramires@users.noreply.github.com>
2023-02-19 18:24:44 -0300

    Add files via upload

---
+++ php/config/parameters.php
@@ -0,0 +1,29 @@
+<?php
+
+return array (
+  'dbHost' => 'localhost',
+  'dbPort' => '3306',
+  'dbUser' => 'pointn18_bdlive',
+  'dbPassword' => 'Kamijo563',
+  'dbName' => 'pointn18_bdlive2',
+  'superUser' => 'admin',
+  'superPass' => 'Kamijo563',
+  'services' => 
+  array (
+    'mailer' => 
+    array (
+      'smtp' => '',
+      'smtpSecure' => 'ssl',
+      'smtpHost' => '',
+      'smtpPort' => '465',
+      'smtpUser' => '',
+      'smtpPass' => '',
+    ),
+  ),
+  'appSettings' => 
+  array (
+    'contactMail' => 'stremon@msn.com',
+  ),
+);
+
+?>
